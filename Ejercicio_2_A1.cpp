df3b55751460effd33c266ec7cfca8a761df3445
Luis Ordoñez <gonzalo.ordonez@unach.edu.ec>
2024-12-05 11:13:33 -0500

    Ejercicio 2 terminado

--- Ejercicio_2_A1.cpp
+++ Ejercicio_2_A1.cpp
@@ -3,13 +3,28 @@ Escribe un programa que solicite al usuario ingresar 10 números enteros entre 1
 almacene en un vector. Usa un bucle para contar cuántas veces aparece cada
 número en el vector (del 1 al 5) y muestra el resultado.
 */
-
 #include <iostream>
 using namespace std;
 int main()
 {
-
-
-
-
+  const int numeros=10;
+  int enteros[numeros];
+  int n_veces[5]={0};
+  cout<<"Ingresa 10 numeros enteros entre 1 - 5"<<endl;
+  for(int i=1;i<=numeros;i++)
+    {
+      cout<<"Entero "<<i<<endl; cin>>enteros[i];
+      while (enteros[i] < 1 || enteros[i] > 5)
+      {
+          cout << "Rango incorrecto ingrese un numero entre 1 - 5: "<<endl;
+          cin >> enteros[i];
+      }
+      n_veces[enteros[i] - 1]++;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"El numero " << i+1 <<" aparece " << n_veces[i]<<" veces" <<endl;
+    }
+system("pause");
+return 0;
 }
